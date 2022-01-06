ab4a66a73535d252e4e569f24c1793b5f7810f57
Prabhjyotg@gmail.com <prabhjyotg@gmail.com>
2022-01-06 09:24:31 -0500

    Fast Api - python framework

--- main.py
+++ main.py
@@ -1,13 +1,45 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
-
-@app.get('/')
+@app.get('/blog?limit=10&published=true')
 def index():
-    return {'data': {'name': 'Romero'}}
+    #only get 10 published blogs
+    return{'data':'blog list'}
+
+@app.get('/blog1')
+def index(limit=10, published: bool = True, sort: Optional[str] = None):
+    if published:
+        return {'data': f'{limit} published blogs from the database'}
+    else:
+        return {'data': f'{limit} blogs from the database'}
 
 
-@app.get('/about')
-def about():
-    return {'data':{'about':'Information'}}
+@app.get('/blog/unpublished')
+def unpublished():
+    return{'data': 'all unpublished blogs'}
+
+
+@app.get('/blog/{id}')
+def show(id: int):
+    # fetch blog with id
+    return {'data': id}
+
+
+@app.get('/blog/{id}/comments')
+def comments(id):
+    # fetch comments of blog with id
+    return {'data': {'1', '2'}}
+
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published_at: Optional[bool]
+
+
+@app.post('/blog')
+def create_blog(request: Blog):
+    return{'data': f"Blog is created with title as {request.title}"}
